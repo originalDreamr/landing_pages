d574538848314ca0e6cfeabe09cb5f2de8c36d53
Firefly <6751880@gmail.com>
2020-02-26 16:09:32 -0500

    add testimonial carousel

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,10 @@
-import {Component, Inject, OnInit, PLATFORM_ID} from '@angular/core';
+import {Component, Inject, OnInit, PLATFORM_ID, ViewChild} from '@angular/core';
 import {SeoService} from '../../services/seo.service';
 import {EventManager} from '@angular/platform-browser';
 import {isPlatformBrowser} from '@angular/common';
+import {NgbCarousel, NgbSlideEvent, NgbSlideEventSource} from '@ng-bootstrap/ng-bootstrap';
+
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-home',
@@ -9,6 +12,14 @@ import {isPlatformBrowser} from '@angular/common';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  constructor(
+    @Inject(PLATFORM_ID) private platformId,
+    private seoService: SeoService,
+    private eventManager: EventManager,
+    ) {
+    this.seoService.setMeta(this.pageConfig);
+    this.isBrowser = isPlatformBrowser(platformId);
+  }
   isBrowser;
 
   pageConfig = {
@@ -18,15 +29,27 @@ export class HomeComponent implements OnInit {
     keywords: '',
     pageUrl: 'https://www.efortles.com',
   };
-  constructor(
-    @Inject(PLATFORM_ID) private platformId,
-    private seoService: SeoService,
-    private eventManager: EventManager,
-    ) {
-    this.seoService.setMeta(this.pageConfig);
-    this.isBrowser = isPlatformBrowser(platformId);
-  }
 
+  slides = [
+    {img: 'assets/home/home_ts-ralph_nee.png', head: 'Testimonials',
+      desc: 'Can’t tell you how happy I am with EFORTles. They helped me replace QuickBooks and TurboTax, ' +
+        'saving at least 30 hours each month of horrible accounting work. Priceless!',
+      name: 'Ralph Nee', position: 'CEO of Guest Tribe Inc.'},
+    {img: 'assets/home/home_ts-edward_kim.png', head: 'Testimonials',
+      desc: 'If it weren’t for EFORTles, I would be totally lost and confused with my company’s financials. ' +
+        'I am so pleased with their services.',
+      name: 'Edward Kim', position: 'Owner of KORE Training Studio'},
+    {img: 'assets/home/home_ts-james_mc_gee.png', head: 'Testimonials',
+      desc: 'Absolutely wonderful! EFORTles lifted the giant accounting burden off my shoulders and ' +
+        'freed up my time so I can focus on growing my business and working my clients.',
+      name: 'James M. McGee', position: 'CEO of Cool Bee POS LLC'},
+    {img: 'assets/home/home_ts-maggie_colangelo.png', head: 'Testimonials',
+      desc: 'EFORTles service is just excellent! Everything is done seamlessly in the background and ' +
+        'now I have perfect business financials all the time.',
+      name: 'Maggie Colangelo', position: 'Partner of Coffee Monster Café'}
+  ];
+
+  @ViewChild('ngCarousel', { static: true }) ngCarousel: NgbCarousel;
 
   ngOnInit(): void {
   }
@@ -35,5 +58,25 @@ export class HomeComponent implements OnInit {
     if (this.isBrowser) {
       window.scrollTo(0, 0);
     }
+  }
+  slideActivate(ngbSlideEvent: NgbSlideEvent) {
+    let activeId: number | string = this.ngCarousel.activeId + 1;
+    // @ts-ignore
+    activeId = activeId > 3 ? 0 : activeId;
+    this.setCarouselIndicator(activeId);
+    // console.log(ngbSlideEvent.source);
+    // console.log(ngbSlideEvent.paused);
+    // console.log(NgbSlideEventSource.INDICATOR);
+    // console.log(NgbSlideEventSource.ARROW_LEFT);
+    // console.log(NgbSlideEventSource.ARROW_RIGHT);
+  }
+  // Move to specific slide
+  navigateToSlide(item) {
+    this.ngCarousel.select(item);
+    this.setCarouselIndicator(this.ngCarousel.activeId);
+  }
+  setCarouselIndicator(index) {
+    $('.carousel-indicators li').removeClass('active');
+    $('.carousel-indicators li').eq(index).addClass('active');
   }
 }
